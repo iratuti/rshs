e943a79e50c769c5417c3017bbdaf9eefbcda6bd
iratuti <iratuti66@gmail.com>
2026-02-28 17:20:19 +0000

    auto-commit for 408ddc36-402d-4e7c-9887-80b02f36ea56

--- nextjs/src/components/ui/toggle.tsx
+++ nextjs/src/components/ui/toggle.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import * as TogglePrimitive from "@radix-ui/react-toggle"
-import { cva } from "class-variance-authority";
+import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils"
 
@@ -28,7 +28,11 @@ const toggleVariants = cva(
   }
 )
 
-const Toggle = React.forwardRef(({ className, variant, size, ...props }, ref) => (
+const Toggle = React.forwardRef<
+  React.ComponentRef<typeof TogglePrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof TogglePrimitive.Root> &
+    VariantProps<typeof toggleVariants>
+>(({ className, variant, size, ...props }, ref) => (
   <TogglePrimitive.Root
     ref={ref}
     className={cn(toggleVariants({ variant, size, className }))}
